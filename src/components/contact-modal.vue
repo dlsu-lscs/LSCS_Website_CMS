27eeefd0b31c78dd9e74f5fc1f30f6bc6d3e9f79
unknown <silentcommentator1216@gmail.com>
2020-10-14 21:38:33 +0800

    Added validation

--- src/components/contact-modal.vue
+++ src/components/contact-modal.vue
@@ -25,7 +25,12 @@
                         </b>
                     </div>
                     <div class="eight wide column">
-                        <form id="contact_us_form" class="ui form" name="contact" method="POST"  netlify>
+                        <form id="contact_us_form" class="ui form"
+                            name="contact"
+                            method="POST"
+                            data-netlify="true"
+                            data-netlify-recaptcha="true"
+                        >
                             <div class="field">
                                 <label>Name</label>
                                 <input type="text" name="name" />
@@ -48,6 +53,7 @@
                                     <i class="times icon"></i>
                                     Cancel
                                 </div>
+                                <div data-netlify-recaptcha="true"></div>
                                 <button type="submit" class="ui positive button">
                                     <i class="paper plane icon"></i>
                                     Submit
@@ -57,57 +63,6 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="ui" :style="{
-                padding: '2rem'
-            }">
-                <div class="column">
-                    <b>
-                        <div><i class="icon facebook f blue"> </i> /LaSalleComputerSociety</div>
-                        <div><i class="icon twitter blue"> </i> /dlsu_LSCS</div>
-                        <div><i class="icon instagram violet"> </i> /dlsu_lscs</div>
-                        <div><i class="icon youtube red"> </i> /LaSalleComputerSociety</div>
-                    </b>
-                </div>
-
-                <div class="column">
-                    <div class="ui header">
-                        Contact us
-                        <g-image class="ui image" src="~/assets/img/macky.png" width="50"/>
-                    </div>
-
-                    <form id="contact_us_form" method="POST" action="/" class="ui form">
-                        <div class="field">
-                            <label>Name</label>
-                            <input type="text" name="name">
-                        </div>
-                        <div class="field">
-                            <label>Subject</label>
-                            <input type="text" name="subject">
-                        </div>
-                        <div class="field">
-                            <label>Email</label>
-                            <input type="text" name="email">
-                        </div>
-                        <div class="field">
-                            <label>Message</label>
-                            <textarea class="opensans" name="message" rows="3"> </textarea>
-                        </div>
-                    </form>
-
-                    <hr>
-
-                    <div class="ui header actions">
-                        <div @click="hide" class="ui negative button">
-                            <i class="times icon"></i>
-                            Cancel
-                        </div>
-                        <div class="ui positive button">
-                            <i class="paper plane icon"></i>
-                            Submit
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </modal>
     </div>
 </template>
